19a9d3c837bb5e1d7f77c8faf76369f114a3fe17
huangqimin <huangqimin001@ke.com>
2020-03-14 01:05:10 +0800

    :sparkles: Support as_row_merge_xls

--- django_excel_storage/excel_storage.py
+++ django_excel_storage/excel_storage.py
@@ -4,7 +4,7 @@ import datetime
 
 from django.core.files.storage import default_storage
 from django.db.models.query import QuerySet
-from django_excel_base import BytesIO, StringIO, as_csv, as_xls, is_py2
+from django_excel_base import BytesIO, StringIO, as_csv, as_row_merge_xls, as_xls, is_py2
 from django_six import Support_ValuesQuerySet, ValuesQuerySet
 
 
@@ -16,8 +16,7 @@ EXCEL_MAXIMUM_ALLOWED_ROWS = 65536
 EXCEL_MAXIMUM_ALLOWED_COLUMN_WIDTH = 65535
 
 
-def __init__(self, data, output_name='excel_data', format='%Y%m%d%H%M%S', headers=None, force_csv=False, encoding='utf-8-sig', font='', sheet_name='Sheet 1', blanks_for_none=True, auto_adjust_width=True):
-
+def __init__(self, data, output_name='excel_data', format='%Y%m%d%H%M%S', headers=None, force_csv=False, encoding='utf-8-sig', font='', sheet_name='Sheet 1', blanks_for_none=True, auto_adjust_width=True, vert=0x01, horz=0x01, row_merge=False):
     self.data = data
     self.output_name = output_name
     self.format = format
@@ -29,6 +28,14 @@ def __init__(self, data, output_name='excel_data', format='%Y%m%d%H%M%S', header
     self.blanks_for_none = blanks_for_none
     self.auto_adjust_width = auto_adjust_width
     self.file_ext = None
+    # VERT_TOP     = 0x00    顶端对齐
+    # VERT_CENTER  = 0x01    居中对齐（垂直方向上）
+    # VERT_BOTTOM  = 0x02    底端对齐
+    # HORZ_LEFT    = 0x01    左端对齐
+    # HORZ_CENTER  = 0x02    居中对齐（水平方向上）
+    # HORZ_RIGHT   = 0x03    右端对齐
+    self.vert = vert
+    self.horz = horz
 
     # Make sure we've got the right type of data to work with
     # ``list index out of range`` if data is ``[]``
@@ -48,9 +55,12 @@ def __init__(self, data, output_name='excel_data', format='%Y%m%d%H%M%S', header
     assert valid_data is True, 'ExcelStorage requires a sequence of sequences'
 
     self.output = StringIO() if is_py2 else BytesIO()
-    # Excel has a limit on number of rows; if we have more than that, make a csv
-    use_xls = True if len(self.data) <= self.EXCEL_MAXIMUM_ALLOWED_ROWS and not self.force_csv else False
-    _, file_ext = (self.as_xls, 'xls') if use_xls else (self.as_csv, 'csv')
+    if row_merge:
+        _, file_ext = (self.as_row_merge_xls, 'xls')
+    else:
+        # Excel has a limit on number of rows; if we have more than that, make a csv
+        use_xls = True if len(self.data) <= self.EXCEL_MAXIMUM_ALLOWED_ROWS and not self.force_csv else False
+        _, file_ext = (self.as_xls, 'xls') if use_xls else (self.as_csv, 'csv')
     self.output.seek(0)
 
     self.file_ext = file_ext
